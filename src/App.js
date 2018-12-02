804653077bdfa581c6c86b08f9a714c88fe8ff96
serhijko <serhijko>
2018-12-02 05:09:46 +0300

    Adding Amount.js with class

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Amount from './components/Amount';
 
 const App = () => (
   <div>
     <h1>Currency Converter</h1>
+    <Amount />
   </div>
 );
 
---
+++ src/components/Amount.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Amount extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amount: 0,
+    };
+  }
+
+  onIncrement = () => {
+    this.setState(state => ({ amount: state.amount + 1 }));
+  };
+
+  onDecrement = () => {
+    this.setState(state => ({ amount: state.amount - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <span>US Dollar: {this.state.amount} </span>
+
+        <button type="button" onClick={this.onIncrement}>
+          +
+        </button>
+        <button type="button" onClick={this.onDecrement}>
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Amount;
