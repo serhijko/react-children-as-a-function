9bb45e9e00b60ea2906e586c72db5f7c5f49ff2d
serhijko <serhijko>
2018-12-02 05:38:35 +0300

    Lifting State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import Amount from './components/Amount';
+import Euro from './components/Euro';
+import Pound from './components/Pound';
 
-const App = () => (
-  <div>
-    <h1>Currency Converter</h1>
-    <Amount />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amount: 0,
+    };
+  }
+
+  onIncrement = () => {
+    this.setState(state => ({ amount: state.amount + 1 }));
+  };
+
+  onDecrement = () => {
+    this.setState(state => ({ amount: state.amount - 1 }));
+  };
+  
+  render() {
+    return (
+      <div>
+        <h1>Currency Converter</h1>
+        <Amount
+          amount={this.state.amount}
+          onIncrement={this.onIncrement}
+          onDecrement={this.onDecrement}
+        />
+
+        <Euro amount={this.state.amount} />
+        <Pound amount={this.state.amount} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Amount.js
+++ src/components/Amount.js
@@ -1,41 +1,16 @@
-import React, { Component } from 'react';
-import Euro from './Euro';
-import Pound from './Pound';
+import React from 'react';
 
-class Amount extends Component {
-  constructor(props) {
-    super(props);
+const Amount = ({ amount, onIncrement, onDecrement }) => (
+  <div>
+    <span>US Dollar: {amount} </span>
 
-    this.state = {
-      amount: 0,
-    };
-  }
-
-  onIncrement = () => {
-    this.setState(state => ({ amount: state.amount + 1 }));
-  };
-
-  onDecrement = () => {
-    this.setState(state => ({ amount: state.amount - 1 }));
-  };
-
-  render() {
-    return (
-      <div>
-        <span>US Dollar: {this.state.amount} </span>
-
-        <button type="button" onClick={this.onIncrement}>
-          +
-        </button>
-        <button type="button" onClick={this.onDecrement}>
-          -
-        </button>
-
-        <Euro amount={this.state.amount} />
-        <Pound amount={this.state.amount} />
-      </div>
-    );
-  }
-}
+    <button type="button" onClick={onIncrement}>
+      +
+    </button>
+    <button type="button" onClick={onDecrement}>
+      -
+    </button>
+  </div>
+);
 
 export default Amount;
