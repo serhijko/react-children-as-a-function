3d3a65e4254a232b287896ad8fbb30de5c44c0fb
serhijko <serhijko>
2018-12-02 05:25:27 +0300

    Adding toFixed(2) method

--- src/components/Euro.js
+++ src/components/Euro.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
+const Euro = ({ amount }) => <p>Euro: {(amount * 0.86).toFixed(2)}</p>;
 
 export default Euro;
--- src/components/Pound.js
+++ src/components/Pound.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
+const Pound = ({ amount }) => <p>Pound: {(amount * 0.76).toFixed(2)}</p>;
 
 export default Pound;
